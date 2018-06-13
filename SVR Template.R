d1fe5863601d785f8a458939d78986d2e30f4833
sguo666 <32041201+sguo666@users.noreply.github.com>
2018-06-13 19:39:22 -0400

    Add files via upload

---
+++ SVR Template.R
@@ -0,0 +1,55 @@
+# SVR
+
+# Setting working directory
+setwd("/Users/Sonya/Downloads/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)")
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[,2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(0)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting the SVR to the dataset
+# install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression') # defult kernel is Gaussian
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the SVR results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            color = 'blue') + 
+  ggtitle('Truth or Bluff (SVR)') + 
+  xlab('Level') + 
+  ylab('Salary')
+
+# Visualising the Regression Model results (for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            color = 'blue') + 
+  ggtitle('Truth or Bluff (Linear Regression)') + 
+  xlab('Level') + 
+  ylab('Salary')
