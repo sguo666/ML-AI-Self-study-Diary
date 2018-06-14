0cb24797d395e9e46840e5636ef62040c33806e0
sguo666 <32041201+sguo666@users.noreply.github.com>
2018-06-13 20:29:33 -0400

    Add files via upload

---
+++ Decision Tree Regression Template.R
@@ -0,0 +1,55 @@
+# Decision Tree Regression
+
+# Setting working directory
+setwd("/Users/Sonya/Downloads/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression")
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[,2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(0)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting the Decision Tree Regression to the dataset
+# install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Decision Tree Regression results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            color = 'blue') + 
+  ggtitle('Truth or Bluff (Decision Tree Regression)') + 
+  xlab('Level') + 
+  ylab('Salary')
+
+# Visualising the Decision Tree Regression results (for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            color = 'blue') + 
+  ggtitle('Truth or Bluff (Decision Tree Regression)') + 
+  xlab('Level') + 
+  ylab('Salary')
